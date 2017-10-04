1a0d1583eec8b13bd678e9cd654502528eb885af
Harrison Liddiard <omniaura5@gmail.com>
2017-10-03 22:48:11 -0700

    easier stateless detection method

--- app.js
+++ app.js
@@ -1,50 +1,18 @@
-const ORIENTATION = {
-  PORTRAIT: 'portrait',
-  LANDSCAPE: 'landscape'
-};
-
 module.exports = callback => {
-  this.navVisible = true;
-  this.orientation = getOrientation();
-  this.updateVisiblity = updateVisiblity.bind(this);
-  this.innerHeight = window.innerHeight;
-  this.innerWidth = window.innerWidth;
+  // PUBLIC
+  this.isLandscape = () => window.innerWidth > window.innerHeight;
+  // bottom bar is shown in portrait when documentElement client height is
+  // equal to window innerHeight. cf. http://ryanve.com/lab/dimensions/
+  this.getBottomBarVisibility = () => !this.isLandscape() && document.documentElement.clientHeight === window.innerHeight;
+  this.destroy = () => window.removeEventListener('resize', listener);
 
-  this.listener = window.addEventListener('resize', event => {
-    // bottom bar is never visible in landscape
-    if (getOrientation() === ORIENTATION.LANDSCAPE) {
-      updateVisiblity(false, callback);
+  // PRIVATE
+  let bottomBarVisible = this.getBottomBarVisibility();
+  const listener = window.addEventListener('resize', event => {
+    // invoke callback if bottom bar visibility has changed
+    if (bottomBarVisible !== this.getBottomBarVisibility()) {
+      bottomBarVisible = this.getBottomBarVisibility();
+      callback(bottomBarVisible);
     }
-    // otherwise we're in portrait; check if width remained constant
-    else if (window.innerWidth === this.innerWidth) {
-      // if new height is shorter, bottom bar has appeared
-      if (window.innerHeight < this.innerHeight) {
-        updateVisiblity(true, callback);
-      }
-      // otherwise it's disappeared
-      else {
-        updateVisiblity(false, callback);
-      }
-    }
-    this.innerHeight = window.innerHeight;
-    this.innerWidth = window.innerWidth;
   });
-
-  this.destroy = () => {
-    window.removeEventListener('resize', this.listener);
-  }
 };
-
-function getOrientation() {
-  return window.innerHeight > window.innerWidth ? 
-    ORIENTATION.PORTRAIT : 
-    ORIENTATION.LANDSCAPE;
-}
-
-// update visibility state and invoke callback if state has changed
-function updateVisiblity(value, callback) {
-  if (value !== this.navVisible) {
-    this.navVisible = value;
-    callback(value);
-  }
-}
